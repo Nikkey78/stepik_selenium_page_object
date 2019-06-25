b090a220bc69aa144ceedfcf7b665a11b7c4b4e9
Nikkey78 <nick-bazh@yandex.ru>
2019-06-25 14:47:07 +0300

    Another method link to login page and test it

--- test_main_page.py
+++ test_main_page.py
@@ -3,19 +3,25 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-# def test_guest_can_ho_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-#     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-#     page.open()                     # открываем страницу
-#     page.should_be_login_link()
-#     # page.go_to_login_page()         # выполняем метод страницы - переходим на страницу логина
+def test_guest_can_ho_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                     # открываем страницу
 
+    # первый подход: возвращаем экз. LoginPage из метода экз. MainPage
+    # login_page = page.go_to_login_page()
+    # login_page.should_be_login_page()
 
-def test_can_login_or_registration(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    login_page = LoginPage(browser, link)
-    login_page.open()
+    # вторй подход:
+    page.go_to_login_page()         # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+# def test_can_login_or_registration(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     login_page = LoginPage(browser, link)
+#     login_page.open()
+#     login_page.should_be_login_page()
 
 
