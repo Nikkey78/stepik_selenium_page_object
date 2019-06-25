0f9c29f0bc3531878e2fba8b407a852a584ea9e8
Nikkey78 <nick-bazh@yandex.ru>
2019-06-25 10:57:13 +0300

    creating first step for project

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action='store', default="en", help="Choose your language")
+
+
+@pytest.fixture
+def browser(request):
+    lang = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
