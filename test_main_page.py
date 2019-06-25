59cd5d834de04a5332eaba1c003d718491a39c60
Nikkey78 <nick-bazh@yandex.ru>
2019-06-25 12:27:42 +0300

    description of the class model for project

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,29 @@
 # import time
+from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_ho_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    # time.sleep(3)
-    login_link.click()
-    # time.sleep(3)
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                     # открываем страницу
+    page.go_to_login_page()         # выполняем метод страницы - переходим на страницу логина
+
+
+# def test_guest_can_go_to_login_page(browser):
+#     browser.get(link)
+#     go_to_login_page(browser)
+
+
+# def go_to_login_page(browser):
+#     link_login_page = browser.find_element_by_css_selector("#login_link")
+#     link_login_page.click()
+
+
+# def test_add_to_cart(browser):
+#     page = ProductPage(url="", browser)   # инициализируем объект Page Object
+#     page.open()                           # открываем страницу в браузере
+#     page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+#     page.add_product_to_cart()            # жмем кнопку добавить в корзину
+#     page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+
+
